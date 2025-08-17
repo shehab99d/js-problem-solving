9b6e96e09c8a39085ec7fc6b2d3139ad7febd748
shehab99b <shehabulislam429@gmail.com>
2025-08-17 22:46:04 +0600

    porblem 3

--- problem.js
+++ problem.js
@@ -7,10 +7,22 @@ function reverse(str) {
 // console.log(reverse("hello"));
 
 
-// Problem 2: 
+// Problem 2: count vowels
 
 function countVowels(str) {
     return str.split("").filter(char => "aeiouAEIOU".includes(char)).length;
 };
 
 // console.log(countVowels("conventional"));
+
+
+// Problem 3: Check for Palindrome
+
+function palindrome(str) {
+    return str === str.split("").reverse().join("");
+};
+
+console.log(palindrome("bab"));
+
+
+
