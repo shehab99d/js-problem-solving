be0b33cfe1272f58f23eefe081003ae26b68c6be
shehab99b <shehabulislam429@gmail.com>
2025-08-17 22:41:14 +0600

    porblem 2

--- problem.js
+++ problem.js
@@ -4,4 +4,13 @@ function reverse(str) {
     return str.split("").reverse().join("");
 };
 
-console.log(reverse("hello"));
+// console.log(reverse("hello"));
+
+
+// Problem 2: 
+
+function countVowels(str) {
+    return str.split("").filter(char => "aeiouAEIOU".includes(char)).length;
+};
+
+// console.log(countVowels("conventional"));
